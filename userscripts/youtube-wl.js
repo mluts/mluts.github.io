f6d62534ae747c8310bfcbf3dbde26fef9930f71
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2018-10-12 12:19:35 +0300

    Add youtube wl userscript

---
+++ userscripts/youtube-wl.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Youtube WL Rand
+// @namespace    http://mluts.github.io/
+// @version      0.1
+// @description  Redirects to random youtube watch-later video
+// @author       Michael Lutsiuk
+// @match        https://www.youtube.com/playlist
+// @updateURL    https://mluts.github.io/userscripts/youtube-wl.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("Hello");
+
+    // Your code here...
+})();
